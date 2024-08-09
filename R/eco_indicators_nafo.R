f9fd36fc15f092ceac54fa15d5bda3c6dbaa3bb1
jamiectam <jamiectam.phd@gmail.com>
2024-08-09 10:51:46 -0300

    +documentation +nafo levels indicators

---
+++ R/eco_indicators_nafo.R
@@ -0,0 +1,19 @@
+#' eco_indicators_nafo
+#'
+#' Estimated Ecological and fishing indicators
+#'
+#' @format 212 rows and 130 columns.
+#'
+#' \itemize{
+#'     \item Var: Shannon diversity.
+#'     \item Value: Value of variable \code{Var}.
+#'     \item Units: Units of variable \code{Var}.
+#'     \item ID: Management Area
+#'     \item YEAR: Year
+#' }
+#'
+#' @details
+#' T. Exact methods
+#' used to derived these data are available at \url{https://publications.gc.ca/site/eng/9.845487/publication.html}.
+#'
+"eco_indicators_nafo"
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MAReco
-Package to support Maritimes Ecosystem Reporting
+
+MAReco is an R data package developed by the Maritimes Ecosystem Based Fisheries Management Working Group for use in Ecosystem Reporting and ecosystem indicator catalog. 
+
+Unless otherwise stated, data are representative of specific Management Areas. 
+
